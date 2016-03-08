895a540d09059128d689f300aff2d87ec7909a24
Mason F. Matthews <mason.f.matthews@gmail.com>
2016-03-08 15:02:45 -0500

    Changes from class on Tuesday.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
   def form
-    
+    if request.post?
+      Survey.create!(name: params[:name],
+        chocolate: params[:chocolate],
+        rainbows: params[:rainbows],
+        puppies: params[:puppies],
+        cash: params[:cold_hard_cash])
+    end
   end
 
 end
